04075227fc6e7f3e1b3865a59f0cfc43bafb83ee
Frigix <44399790+Frigix@users.noreply.github.com>
2026-05-18 23:10:09 +0200

    Update data.js
    uh ouais

--- data.js
+++ data.js
@@ -320,5 +320,229 @@ const characters = [
     {
         name: 'Jerry',
         image: './img/chars/Jerry.webp'
-    }                              
+    },                              
+   {
+        name: 'Egir Ranpak',
+        image: './img/chars/abyss.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Egir_Ranpak',
+    },
+   {
+        name: 'Edgie (Chaussette)',
+        image: './img/chars/edgie le vrai.png',
+    },
+   {
+        name: 'Liam Aebischer',
+        image: './img/chars/employedumois.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Liam_Aebischer',
+    },
+   {
+        name: 'Chiminigagua (Âge des Dieux)',
+        image: './img/chars/Chimi.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Chiminigagua',
+    },
+   {
+        name: 'Luxia don Mado',
+        image: './img/chars/Luxia_don_Mado.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Luxia_don_Mado',
+    },
+   {
+        name: 'Garry Flunch',
+        image: './img/chars/Garry_Flunch.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Garry_Flunch',
+    },
+   {
+        name: 'Alex Heisenberg',
+        image: './img/chars/Blue_Rose.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Alex_Heisenberg',
+    },
+   {
+        name: 'Rika Yoshihara',
+        image: './img/chars/Rika.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Rika_Yoshihara',
+    },
+   {
+        name: 'Gramoriana Visconte',
+        image: './img/chars/Gramoriana_Visconte.png',
+    },
+   {
+        name: 'Chao Liu',
+        image: './img/chars/Chao_Liu.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Chao_Liu',
+    },
+   {
+        name: 'Gabriel P. Price',
+        image: './img/chars/Gabriel avec ailes.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Gabriel P. Price',
+    },
+   {
+        name: 'Cryo Reaper',
+        image: './img/chars/Cyro_Reaper.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Cryo_Reaper',
+    },
+   {
+        name: 'Shidare-no-Miya Yanagihito',
+        image: './img/chars/Yanagi.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Shidare-no-Miya_Yanagihito',
+    },
+   {
+        name: 'Olga Koshkyn',
+        image: './img/chars/Olga.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Olga_Koshkyn',
+    },
+   {
+        name: 'Ivan',
+        image: './img/chars/Ivan.jpeg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Ivan',
+    },
+   {
+        name: 'Su-Cheol Bari',
+        image: './img/chars/su-cheol.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Su-Cheol_Bari',
+    },
+   {
+        name: 'Asume Koimoto',
+        image: './img/chars/Asume_Koimoto.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Asume_Koimoto',
+    },
+   {
+        name: 'Buster K.',
+        image: './img/chars/Buster.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Buster_K.',
+    },
+   {
+        name: 'Anastasie Sophia Baphomet',
+        image: './img/chars/Anastasie4.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Anastasie_Baphomet',
+    },
+   {
+        name: 'Debbie',
+        image: './img/chars/Debbie.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Debbie',
+    },
+   {
+        name: 'Vanita Parlombra Oralistra',
+        image: './img/chars/vanita.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Vanita_Parlombra_Oralistra',
+    },
+   {
+        name: 'Sujita Hina',
+        image: './img/chars/sujita_hina.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Sujita_Hina',
+    },
+   {
+        name: 'Steven Candish',
+        image: './img/chars/steven_candish.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Steven_Candish',
+    },
+   {
+        name: 'Kirino Lee',
+        image: './img/chars/Kirino_Lee.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Kirino_Lee',
+    },
+   {
+        name: 'Federica von Madomeister-Hoffmann',
+        image: './img/chars/Morb_Jr_wow.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Federica_von_Madomeister-Hoffmann',
+    },
+   {
+        name: 'Carlito',
+        image: './img/chars/CARLITO2.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Carlito',
+    },
+   {
+        name: 'Aldegrin de Pairaud',
+        image: './img/chars/Aldegrin.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Aldegrin_de_Pairaud',
+    },
+   {
+        name: 'Frigix (Ancient Sands)',
+        image: './img/chars/FrigixAS.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Frigix_(Ancient_Sands)',
+    },
+   {
+        name: 'Jonn II Malop',
+        image: './img/chars/jonn_2_malop.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Jonn_II_Malop',
+    },
+   {
+        name: 'Eleonóra Ahrozný',
+        image: './img/chars/eleonora.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Eleonóra_Ahrozný',
+    },
+   {
+        name: 'Rister',
+        image: './img/chars/Rister.png',
+    },
+   {
+        name: 'Amelia',
+        image: './img/chars/Amelia.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Amelia',
+    },
+   {
+        name: 'Liam “Justin” Life',
+        image: './img/chars/liam_life.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Liam_Life',
+    },
+   {
+        name: 'Louis Sarkozy',
+        image: './img/chars/louis_sarkozy.png',
+    },
+   {
+        name: 'Antonio Celerán',
+        image: './img/chars/pretty_boy.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Antonio_Celerán',
+    },
+   {
+        name: 'Alastasia Prismane Velestor',
+        image: './img/chars/prismane.jpg',
+    },
+   {
+        name: 'El Caballero',
+        image: './img/chars/el_caballero.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/El_Caballero',
+    },
+   {
+        name: 'Luke Raymonde',
+        image: './img/chars/Luke_Raymonde.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Luke_Raymonde',
+    },
+   {
+        name: 'Lilith Newgate',
+        image: './img/chars/Lilith_Newgate.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Lilith_Newgate',
+    },
+   {
+        name: 'Spielberg K.',
+        image: './img/chars/spil.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Spielberg_K.',
+    },
+   {
+        name: 'Sam Lee',
+        image: './img/chars/Sam_Lee.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Sam_Lee',
+    },
+   {
+        name: 'Baghi',
+        image: './img/chars/baghi.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Baghi',
+    },
+   {
+        name: 'Carlitwo',
+        image: './img/chars/Carlitwo.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Carlitwo',
+    },
+   {
+        name: 'Louvre Lagamme',
+        image: './img/chars/louvre_magius.png',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Louvre_Lagamme',
+    },
+   {
+        name: 'Aruki Ren',
+        image: './img/chars/Aruki_Ren.jpg',
+        wiki: 'https://servantfarm.fandom.com/fr/wiki/Aruki_Ren',
+    },
+   {
+        name: 'Hana Trump & Spencer Mill',
+        image: './img/chars/hana_et_spencer.png',
+    }
 ]

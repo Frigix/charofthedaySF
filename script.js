0a8759d5af23aac1cc90536f1210d777e5ef286a
arthur.valin <arthur.valin@etu.uca.fr>
2026-05-18 22:20:30 +0200

    add sf dex

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+const LOCAL_DATA_KEY = 'localData';
+
 const picture = document.getElementById('picture');
 const wiki = document.getElementById('wiki');
+const sfDex = document.getElementById('sfDex');
+const sfDexCount = document.getElementById('sfDexCount');
 const charname = document.getElementById('name');
 const rollBtn = document.getElementById('rollBtn');
 const today = new Date().toDateString();
-const savedCharacter = localStorage.getItem('character');
-const savedDate = localStorage.getItem('date');
+const rawLocalData = localStorage.getItem(LOCAL_DATA_KEY);
+const localData = rawLocalData ? JSON.parse(rawLocalData) : [];
 
 function displayCharacter (character) {
     picture.src = character.image;
@@ -17,23 +21,81 @@ function displayCharacter (character) {
     charname.innerText = character.name;
 }
 
-function saveCharacter (character) {
-    localStorage.setItem('character', JSON.stringify(character));
-    localStorage.setItem('date', today)
+function saveCharacter(character) {
+    localData.unshift(
+        {
+            id: character.name,
+            date: today
+        } 
+    )
+
+    localStorage.setItem(LOCAL_DATA_KEY, JSON.stringify(localData));
 }
 
-if (savedCharacter && savedDate === today) {
-    const character = JSON.parse(savedCharacter);
-    displayCharacter(character);
+function disableRollButton() {
     rollBtn.disabled = true;
     rollBtn.innerText = 'Come back tomorrow'
+}
+
+function enableRollButton() {
+    rollBtn.disabled = false;
+    rollBtn.innerText = 'Roll'
+}
+
+function displaySFDex() {
+    var total = 0;
+    var totalObtained = 0;
+
+  characters.forEach(it => {    
+    let localItem = localData.find(localIt => localIt.id === it.name);
+
+    let item = document.createElement('div');
+    item.classList.add('sfDexItem');
+
+    const img = document.createElement('img');
+    img.src = localItem ? it.image : "./img/unknown.jpg";
+    img.alt = it.name;
+
+    const label = document.createElement('div');
+    label.classList.add('label');
+    label.textContent = localItem ? it.name : "Not yet obtained";
+
+    item.appendChild(img);
+    item.appendChild(label);
+
+    sfDex.appendChild(item);
+
+    total++;
+    if (localItem) {
+        totalObtained++;
+    }
+  });
+
+  let isCompleted = total === totalObtained;
+  var countText = `${totalObtained}/${total}`;
+  if (isCompleted) {
+    countText =  `🎉 ${countText} 🎉`;
+  }
+
+  sfDexCount.innerText = countText;
+}
+
+if (localData.length && localData[0].date === today) {
+    let characterId = localData[0].id;
+    let character = characters.find(it => it.name === characterId); 
+    
+    displayCharacter(character);
+    disableRollButton();
+} else {
+    enableRollButton();
 }
 
 rollBtn.onclick = function () {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    const character = characters[randomIndex];
-    displayCharacter(character);
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    let character = characters[randomIndex];
+
     saveCharacter(character);
-    rollBtn.disabled = true;
-    rollBtn.innerText = 'Come back tomorrow'
+    location.reload();
 }
+
+displaySFDex();
